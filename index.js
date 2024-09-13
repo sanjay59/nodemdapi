e55828d1a7757bb64d2e8c5fe4b2521f32cb938a
sanjay59 <sanjayyadav224210@gmail.com>
2024-09-13 10:27:06 +0530

    change done

--- index.js
+++ index.js
@@ -10,12 +10,15 @@ require("./routes/api.js")(app)
 
 
  
+app.get('/', (req, res) => { 
+    res.send('Hello, Azure! This is a Node.js application.'); 
+  });
 
 app.get('*', (req, res) => {
     res.send("Something wrong!");
 })
 
-let PORT = 3000
+let PORT = process.env.PORT || 3000
 app.listen(PORT, () =>{
     console.log(`Server is running on port `+PORT);
 })
