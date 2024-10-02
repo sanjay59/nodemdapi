13ba73021b1118a94a975eb9430a28b9bee8ab90
sanjay59 <sanjayyadav591996@gmail.com>
2024-10-02 18:11:16 +0530

    m disable aaa

--- index.js
+++ index.js
@@ -3,12 +3,6 @@ const express = require("express");
 
 const app = express();
 app.use(express.json());
-// const Profile = require("./app/models/Profile.js");
-
-// require("./db.js");
-// require("./routes/api.js")(app)
-
-
  
 app.get('/', (req, res) => { 
     res.send('Hello, Azure! This is a Node.js application.'); 
